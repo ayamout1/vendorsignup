2e9d7eba3373a5fbaf2b82bb6ce572b83a6a4462
ayamout1 <105465622+ayamout1@users.noreply.github.com>
2023-12-28 11:17:53 -0600

    adding email view

---
+++ resources/views/emails/vendor_agreement.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signed Vendor Agreement</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+        }
+    </style>
+</head>
+<body>
+    <h1>Signed Vendor Agreement</h1>
+
+    <p>Hello,</p>
+    <p>We have received and processed your vendor agreement. Please find the attached document for your records.</p>
+
+    <!-- You can add more personalized information here using Blade templating -->
+    <p>Vendor Name: {{ $vendor_name }}</p>
+    <p>Owner Name: {{ $owner_name }}</p>
+    <p>Agreement Date: {{ date('F d, Y') }}</p>
+
+    <p>Thank you for your prompt attention to this matter.</p>
+
+    <p>Best Regards,<br>
+    The Procurement Team</p>
+</body>
+</html>

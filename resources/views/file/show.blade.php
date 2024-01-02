fa121f833178c2a43ed1992d99f9f0c21efb7dab
ayamout1 <105465622+ayamout1@users.noreply.github.com>
2024-01-02 14:43:34 -0600

    sessions

--- resources/views/file/show.blade.php
+++ resources/views/file/show.blade.php
@@ -6,6 +6,8 @@
     <title>Edit Uploaded Files</title>
     <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
 </head>
 <body>
     <div class="container mt-5">

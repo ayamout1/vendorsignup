bd0d7e829f95217dc95e953dd7e1a43c6b3ba210
ayamout1 <105465622+ayamout1@users.noreply.github.com>
2024-01-08 07:30:55 -0600

    name change

--- resources/views/components/layouts/front.blade.php
+++ resources/views/components/layouts/front.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'TYS Global Partners')</title>
+    <title>@yield('title', 'TYSGlobal Partners')</title>
 
     <!-- Livewire Styles -->
     @livewireStyles

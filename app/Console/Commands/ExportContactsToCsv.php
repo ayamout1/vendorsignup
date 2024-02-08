5b21342fc73969b2fbcdb620a24a5adf1d637543
ayamout1 <105465622+ayamout1@users.noreply.github.com>
2024-02-08 09:33:45 -0600

    vendor name ->vendor name sqlite

--- app/Console/Commands/ExportContactsToCsv.php
+++ app/Console/Commands/ExportContactsToCsv.php
@@ -38,7 +38,7 @@ class ExportContactsToCsv extends Command
             ->join('vendors as v', 'c.vendor_id', '=', 'v.id')
             ->select(
                 'c.id',
-                'v.name as vendor_name',
+                'v.vendor_name as vendor_name',
                 'c.contact_name',
                 'c.contact_email',
                 'c.contact_phone',

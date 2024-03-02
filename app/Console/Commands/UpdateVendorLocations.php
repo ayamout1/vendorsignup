c266135c521157bf2d56851f2e7fb64cba28855e
ayamout1 <105465622+ayamout1@users.noreply.github.com>
2024-03-02 15:57:10 -0600

    updated table to cstm

--- app/Console/Commands/UpdateVendorLocations.php
+++ app/Console/Commands/UpdateVendorLocations.php
@@ -3,58 +3,53 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use DB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
-class UpdateVendorLocations extends Command
+class GeocodeVendors extends Command
 {
-    protected $signature = 'update:vendor-locations';
-    protected $description = 'Update vendor locations with latitude and longitude';
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    protected $signature = 'geocode:vendors';
+    protected $description = 'Geocode vendor addresses and update their lat/lng in the database';
 
     public function handle()
     {
-        $vendors = DB::connection('suitecrm')
-            ->table('vsf_vendornetwork')
-            ->whereNull('longitude_c') // Assuming you want to update records without longitude and latitude
-            ->orWhereNull('latitude_c')
+        $vendors = DB::connection('suitecrm')->table('vsf_vendornetwork')
+            ->leftJoin('vsf_vendornetwork_cstm', 'vsf_vendornetwork.id', '=', 'vsf_vendornetwork_cstm.id_c')
+            ->select('vsf_vendornetwork.*', 'vsf_vendornetwork_cstm.longitude_c', 'vsf_vendornetwork_cstm.latitude_c')
+            ->whereNull('vsf_vendornetwork_cstm.longitude_c') // Assuming you want to update vendors without longitude
+            ->orWhereNull('vsf_vendornetwork_cstm.latitude_c') // or latitude
             ->get();
 
         foreach ($vendors as $vendor) {
             $address = "{$vendor->address_c}, {$vendor->city_c}, {$vendor->state_c} {$vendor->postal_c}";
-            $geocode = $this->geocodeAddress($address);
-            if ($geocode) {
-                DB::connection('suitecrm')
-                    ->table('vsf_vendornetwork')
-                    ->where('id', $vendor->id)
-                    ->update([
-                        'longitude_c' => $geocode['lng'],
-                        'latitude_c' => $geocode['lat'],
-                    ]);
+            $geoData = $this->geocodeAddress($address);
+
+            if ($geoData) {
+                DB::connection('suitecrm')->table('vsf_vendornetwork_cstm')
+                    ->updateOrInsert(
+                        ['id_c' => $vendor->id], // Ensure correct association
+                        [
+                            'longitude_c' => $geoData['lng'],
+                            'latitude_c' => $geoData['lat'],
+                        ]
+                    );
             }
         }
     }
 
     protected function geocodeAddress($address)
     {
-        $apiKey = env('AIzaSyD18r4JGshadpsZLRzn3UfeH2WsMUEJ0Fc'); // Store your API key in .env file
+        $apiKey = env('GOOGLE_MAPS_API_KEY');
         $response = Http::get("https://maps.googleapis.com/maps/api/geocode/json", [
             'address' => $address,
             'key' => $apiKey,
         ]);
 
-        if ($response->successful()) {
-            $json = $response->json();
-            if (!empty($json['results'])) {
-                return [
-                    'lat' => $json['results'][0]['geometry']['location']['lat'],
-                    'lng' => $json['results'][0]['geometry']['location']['lng'],
-                ];
-            }
+        if ($response->successful() && !empty($response->json('results'))) {
+            return [
+                'lat' => $response->json('results.0.geometry.location.lat'),
+                'lng' => $response->json('results.0.geometry.location.lng'),
+            ];
         }
 
         return null;
